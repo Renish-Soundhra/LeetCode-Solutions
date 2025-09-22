ac71e36a039c73b43d51661358ac6598c0622027
Renish-Soundhra <168426104+Renish-Soundhra@users.noreply.github.com>
2025-09-22 23:00:56 +0530

    Time: 0 ms (100%), Space: 40.7 MB (91.09%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        while(n>1){
+            if(n%4==0){
+                n/=4;
+            }
+            else{
+                break;
+            }
+        }
+        if(n==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
