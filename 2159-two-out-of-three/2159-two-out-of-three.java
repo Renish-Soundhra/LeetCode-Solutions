69b5d8aaad41502280e9ed8f6c83e9666faabca8
Renish-Soundhra <168426104+Renish-Soundhra@users.noreply.github.com>
2025-10-04 22:51:32 +0530

    Time: 7 ms (41.28%), Space: 45.1 MB (37.42%) - LeetHub

---
+++ 2159-two-out-of-three/2159-two-out-of-three.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        HashSet<Integer> set1=new HashSet<>();
+        for(int i:nums1){
+            set1.add(i);
+        }
+        HashSet<Integer> set2=new HashSet<>();
+        for(int i:nums2){
+            set2.add(i);
+        }
+        HashSet<Integer> set3=new HashSet<>();
+        for(int i:nums3){
+            set3.add(i);
+        }
+        for(int i:set1){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int i:set2){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int i:set3){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        List<Integer> list=new ArrayList<>();
+        for(int i:map.keySet()){
+            if(map.get(i)>=2){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
