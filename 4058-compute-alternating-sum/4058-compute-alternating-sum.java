a0a5cc807fd6db908c0e2af8bedf2ff65e688f24
Renish-Soundhra <168426104+Renish-Soundhra@users.noreply.github.com>
2025-11-01 21:42:21 +0530

    Time: 0 ms (100%), Space: 46.6 MB (5.19%) - LeetHub

---
+++ 4058-compute-alternating-sum/4058-compute-alternating-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int odd=0;
+        int even=0;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0){
+                even+=nums[i];
+            }
+            else{
+                odd+=nums[i];
+            }
+        }
+        sum=even-odd;
+        return sum;
+    }
+}
