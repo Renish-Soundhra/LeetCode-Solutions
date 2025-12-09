047bf55086124666f92d67b2b72b23286eaea5ae
Renish-Soundhra <168426104+Renish-Soundhra@users.noreply.github.com>
2025-12-09 09:18:59 +0530

    Time: 0 ms (100%), Space: 43.5 MB (35.53%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,34 +1,19 @@
 class Solution {
-    public int fun(int r,int c,int dp[][],int grid[][]){
-        for(int i=1;i<r;i++){
-            for(int j=1;j<c;j++){
-                if(grid[i][j]!=1){
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                }
-            }
-        }
-        return dp[r-1][c-1];
-    }
     public int uniquePathsWithObstacles(int[][] grid) {
         int r=grid.length;
         int c=grid[0].length;
-        int dp[][]=new int[r][c];
+        int dp[]=new int[c];
+        dp[0]=(grid[0][0]==1?0:1);
         for(int i=0;i<r;i++){
-            if(grid[i][0]!=1){
-                dp[i][0]=1;
-            }
-            else{
-                break;
-            }
-        }
-        for(int i=0;i<c;i++){
-            if(grid[0][i]!=1){
-                dp[0][i]=1;
-            }
-            else{
-                break;
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1){
+                    dp[j]=0;
+                }
+                else if(j>0){
+                    dp[j]+=dp[j-1];
+                }
             }
         }
-        return fun(r,c,dp,grid);
+        return dp[c-1];
     }
 }
