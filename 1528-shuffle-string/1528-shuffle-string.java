07d59970fc5e541351fce9c2133438baa4e05561
Renish-Soundhra <168426104+Renish-Soundhra@users.noreply.github.com>
2025-12-22 23:17:49 +0530

    Time: 1 ms (60.59%), Space: 45.2 MB (81.98%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char ans[]=new char[indices.length];
+        for(int i=0;i<ans.length;i++){
+            ans[indices[i]]=s.charAt(i);
+        }
+        return new String(ans);
+    }
+}
