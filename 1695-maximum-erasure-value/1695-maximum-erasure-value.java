b70ca993983476c1a9cc474f8d2aeb64f01e19da
Renish-Soundhra <168426104+Renish-Soundhra@users.noreply.github.com>
2026-02-01 18:01:15 +0530

    Time: 90 ms (8.54%), Space: 94.8 MB (14.44%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -4,21 +4,16 @@ class Solution {
         int i=0;
         int j=0;
         int max=0;
-        int start=0;
-        int end=0;
+        int sum=0;
         while(i<nums.length){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            sum+=nums[i];
             while(map.get(nums[i])>1){
+                sum-=nums[j];
                 map.put(nums[j],map.get(nums[j])-1);
                 j++;
             }
             i++;
-            start=j;
-            end=i;
-            int sum=0;
-            for(int k=start;k<end;k++){
-                sum+=nums[k];
-            }
             max=Math.max(max,sum);
         }
         return max;
