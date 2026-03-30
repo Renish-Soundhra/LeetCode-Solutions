065d8df6072c9c30071f4bcf03aa40e69e65ff68
Renish-Soundhra <168426104+Renish-Soundhra@users.noreply.github.com>
2026-03-30 13:13:41 +0530

    Time: 38 ms (84.62%), Space: 46.5 MB (42.86%) - LeetHub

--- 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.java
+++ 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.java
@@ -3,12 +3,15 @@ class Solution {
         int mod=1000000007;
         int n=types.length;
         int dp[][]=new int[n+1][target+1];
-        dp[0][0]=1;
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
         for(int i=1;i<=n;i++){
             int count=types[i-1][0];
             int mark=types[i-1][1];
-            for(int j=0;j<=target;j++){
-                for(int k=0;k<=count;k++){
+            for(int j=1;j<=target;j++){
+                dp[i][j]=dp[i-1][j];
+                for(int k=1;k<=count;k++){
                     if(j-k*mark>=0){
                         dp[i][j]=(dp[i][j]+dp[i-1][j-k*mark])%mod;
                     }
