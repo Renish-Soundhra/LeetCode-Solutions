dde92ad5570ab9a5281a46f8c005c085587c4b42
Renish-Soundhra <168426104+Renish-Soundhra@users.noreply.github.com>
2026-03-31 11:25:28 +0530

    Time: 42 ms (82.03%), Space: 46.4 MB (32.03%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.java
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.java
@@ -1,19 +1,16 @@
 class Solution {
     int mod=1000000007;
-    int dp[][];
+    Integer dp[][];
     public int countRoutes(int[] locations, int start, int finish, int fuel) {
         int n=locations.length;
-        dp=new int[n][fuel+1];
-        for(int i=0;i<n;i++) {
-            Arrays.fill(dp[i],-1);
-        }
+        dp=new Integer[n][fuel+1];
         return dfs(locations,start,finish,fuel);
     }
     private int dfs(int loc[],int cur,int finish,int fuel){
         if(fuel<0){
             return 0;
         }
-        if(dp[cur][fuel]!=-1){
+        if(dp[cur][fuel]!=null){
             return dp[cur][fuel];
         }
         int ways=(cur==finish)?1:0;
